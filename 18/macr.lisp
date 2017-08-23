abcf0986ef11b8226006e49ece833831c07574d5
velengel <kounakadora528@gmail.com>
2017-08-23 09:27:26 +0900

    12

--- 18/macr.lisp
+++ 18/macr.lisp
@@ -1,4 +1,14 @@
 (defmacro lazy (&body body)
   (let ((forced (gensym))
 	(value (gensym)))
-    `(let ((,for
+    `(let ((,forced nil)
+	   (,value nil))
+       (lambda ()
+	 (unless ,forced
+	   (setf ,value (progn ,@body))
+	   (setf ,forced t))
+	 ,value))))
+
+(defun force (lazy-value)
+  (funcall lazy-value))
+
